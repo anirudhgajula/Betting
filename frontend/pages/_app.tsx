d9466babc835a96ccdbee73d0f976ed75ac44ff7
anirudhgajula <gajula.ani@gmail.com>
2022-12-16 17:17:45 +0800

    Updates to _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import {
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+
+import { mainnet, goerli, sepolia} from 'wagmi/chains';
+import { infuraProvider } from 'wagmi/providers/infura';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [mainnet, goerli, sepolia],
+  [
+    infuraProvider({ apiKey: process.env.INFURA_API_KEY as string }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Betting Contract',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
