33e454b51f54747cc85d8240b132fe2916b7fca8
anirudhgajula <gajula.ani@gmail.com>
2022-12-20 11:20:02 +0800

    Updated config.js file

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const bettingContractAddress = "0x6A5b8fA90C2A9Bd936c8b77775cf010Fa6b94D11"
-export const tokenContractAddress = "0x0533BFc325d1F6d18AAF399c1fb434a142B63aa8"
+export const bettingContractAddress = "0xb6682c3cE9D1c39e06DD09110B02cAa6361358D3"
+export const tokenContractAddress = "0xE97683172FBdDAd7275B454375Fa7F678Cb23C5E"
